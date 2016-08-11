a4344e0bd37c46f65c19502e1f2eae12f0d736ce
Horacio <dar5_0094@hotmail.com>
2016-08-10 22:20:32 -0500

    Inicialización de la creación de los Services

---
+++ src/edu/upqroo/reservations/services/CostumerServiceImpl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upqroo.reservations.services;
+
+/**
+ *
+ * @author Horacio
+ */
+public class CostumerServiceImpl implements CostumerService{
+    
+}

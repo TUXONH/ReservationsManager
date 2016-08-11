eb45516dbc70640f3718f7f259164fb6dcf78659
Horacio <horacioespinosa94@gmail.com>
2016-08-10 21:05:53 -0500

    Creación del proyecto ReservationsManeger

---
+++ src/edu/upqroo/reservations/app/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upqroo.reservations.app;
+
+/**
+ *
+ * @author Horacio
+ */
+public class Principal {
+    public static void main(String[] args){
+        System.out.println("Hi reservation manager!");
+    }
+}

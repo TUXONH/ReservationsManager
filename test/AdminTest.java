29cdf5d4f81250df9ad5fde8d633599c972d0853
moises <moises.b.k@hotmai.com>
2016-08-15 03:05:14 -0500

    Test Admin

--- test/AdminTest.java
+++ test/AdminTest.java
@@ -4,7 +4,20 @@
  * and open the template in the editor.
  */
 
+import edu.upqroo.reservations.daos.ReservationsDao;
+import edu.upqroo.reservations.daos.ReservationsMongoDao;
+import edu.upqroo.reservations.daos.UsersDao;
+import edu.upqroo.reservations.daos.UsersMongoDao;
+import edu.upqroo.reservations.domain.TableResults;
+import edu.upqroo.reservations.domain.Users;
 import edu.upqroo.reservations.exceptions.IfCostumerExistsException;
+import edu.upqroo.reservations.exceptions.IfLimitUsersException;
+import edu.upqroo.reservations.exceptions.IfUsersExistsException;
+import edu.upqroo.reservations.services.ReservationService;
+import edu.upqroo.reservations.services.ReservationServiceImpl;
+import edu.upqroo.reservations.services.UserService;
+import edu.upqroo.reservations.services.UserServiceImpl;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -42,8 +55,138 @@ public class AdminTest {
     //
     // @Test
     // public void hello() {}
-    @Test(expected = IfCostumerExistsException.class)
-    public void AddEmployeeRepitExistsException() throws IfCostumerExistsException{
+    @Test(expected = IfUsersExistsException.class)
+    public void AddEmployeeRepitExistsException() throws IfUsersExistsException, IfLimitUsersException{
+            UsersDao con;
+            UserService userService;
+            ReservationsDao dao;
+            ReservationService Reservacion;
+            con = new UsersMongoDao();
+            dao = new ReservationsMongoDao();
+            userService = new UserServiceImpl(con,dao);
+            Users user = new Users();
+            user.setName("Moi");
+            user.setLastName("Barrera");
+            user.setUserName("Mojojo");
+            user.setPassword("Hola");
+            user.setIsAdministrator(false);
+            userService.addUser(user);
+            userService.addUser(user);
+    }
     
+    @Test(expected = IfLimitUsersException.class)
+    public void LimitAddExistsException() throws IfUsersExistsException, IfLimitUsersException{
+            UsersDao con;
+            UserService userService;
+            ReservationsDao dao;
+            ReservationService Reservacion;
+            con = new UsersMongoDao();
+            dao = new ReservationsMongoDao();
+            userService = new UserServiceImpl(con,dao);
+            Users user = new Users();
+            user.setName("Moi");
+            user.setLastName("Barrera");
+            user.setUserName("Mojojo");
+            user.setPassword("Hola");
+            user.setIsAdministrator(false);
+            userService.addUser(user);
+            user.setUserName("1");
+            userService.addUser(user);
+            user.setUserName("2");
+            userService.addUser(user);
+            user.setUserName("3");
+            userService.addUser(user);
+            user.setUserName("4");
+            userService.addUser(user);
+            user.setUserName("5");
+            userService.addUser(user);
+            user.setUserName("6");
+            userService.addUser(user);
+            user.setUserName("7");
+            userService.addUser(user);
+            user.setUserName("8");
+            userService.addUser(user);
+            user.setUserName("9");
+            userService.addUser(user);
+            user.setUserName("10");
+            userService.addUser(user);
+    }
+    
+    @Test
+    public void DeleteUser() throws IfUsersExistsException, IfLimitUsersException
+    {
+            UsersDao con;
+            UserService userService;
+            ReservationsDao dao;
+            ReservationService Reservacion;
+            con = new UsersMongoDao();
+            dao = new ReservationsMongoDao();
+            userService = new UserServiceImpl(con,dao);
+            Users user = new Users();
+            user.setName("Moi");
+            user.setLastName("Barrera");
+            user.setUserName("Mojojo");
+            user.setPassword("Hola");
+            user.setIsAdministrator(false);
+            //userService.addUser(user);
+            userService.DeleteUser(2);
+    }
+    
+    @Test
+    public void UpdateUser() throws IfUsersExistsException, IfLimitUsersException{
+            UsersDao con;
+            UserService userService;
+            ReservationsDao dao;
+            ReservationService Reservacion;
+            con = new UsersMongoDao();
+            dao = new ReservationsMongoDao();
+            userService = new UserServiceImpl(con,dao);
+            Users user = new Users();
+            user.setId(5);
+            user.setName("Moi");
+            user.setLastName("Barrera");
+            user.setUserName("jojo");
+            user.setPassword("Hola");
+            user.setIsAdministrator(false);
+            //userService.addUser(user);
+            userService.UpdateUser(user);
+    }  
+    
+    @Test
+    public void GetUser() throws IfUsersExistsException, IfLimitUsersException{
+            UsersDao con;
+            UserService userService;
+            ReservationsDao dao;
+            ReservationService Reservacion;
+            con = new UsersMongoDao();
+            dao = new ReservationsMongoDao();
+            userService = new UserServiceImpl(con,dao);
+            Users user = new Users();
+            user.setName("Moi");
+            user.setLastName("Barrera");
+            user.setUserName("Mojojo");
+            user.setPassword("Hola");
+            user.setIsAdministrator(false);
+            //userService.addUser(user);
+            userService.GetUser(3);
+    }   
+  
+    @Test
+    public void GetListReservationsByUser() throws IfUsersExistsException, IfLimitUsersException{
+            UsersDao con;
+            UserService userService;
+            ReservationsDao dao;
+            ReservationService Reservacion;
+            con = new UsersMongoDao();
+            dao = new ReservationsMongoDao();
+            userService = new UserServiceImpl(con,dao);
+            Users user = new Users();
+            user.setName("Moi");
+            user.setLastName("Barrera");
+            user.setUserName("Mojojo");
+            user.setPassword("Hola");
+            user.setIsAdministrator(false);
+            //userService.addUser(user);
+            List<TableResults>  result = userService.GetResultVentas();
     }
 }
